419d75b981b9b14e8308690b415ffc6e2ad20e18
Josh Thomas <josh@ionic.io>
2019-06-14 10:42:39 -0500

    missed extra line.

--- test-app/src/main.ts
+++ test-app/src/main.ts
@@ -2,15 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import { defineCustomElements, applyPolyfills } from 'test-components/loader';
 
+Vue.config.productionTip = false;
+Vue.config.ignoredElements = [/my-\w*/];
+
 // Apply necessary Polyfills for IE11.
 applyPolyfills().then(() => {
   defineCustomElements(window);
 });
-
-Vue.config.productionTip = false;
-Vue.config.ignoredElements = [/my-\w*/];
-
-defineCustomElements(window);
 
 new Vue({
   render: (h) => h(App),
